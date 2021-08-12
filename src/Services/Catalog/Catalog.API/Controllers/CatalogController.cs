2e416ca3e6629dd162397a2e97c0e9836fd2f82a
H.M.Zahid <muhammad.zahid@vicenna.com>
2021-08-12 08:47:00 +0500

    Creating Catalog controller V # 21

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController: ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+       
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _repository.GetProducts();
+        }
+    }
+}
